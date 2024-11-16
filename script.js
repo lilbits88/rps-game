b19641da216727d70e2bac48dbc7f0230afd86e9
Jonathan Anleu <jonathan.anleu@gmail.com>
2024-11-15 23:37:57 -0600

    updating game

--- script.js
+++ script.js
@@ -1,62 +1,33 @@
-/*const getComputerChoice = () => {
-  const randomNum = Math.floor(Math.random() * 3) + 1;
-
-  if (randomNum === 1){
-    return "ROCK"
-  }
-  else if (randomNum === 2) {
-    return "PAPER"
-  }
-  else {
-    return "SCISSORS"
-  }
-}
-
-
-const getHumanChoice = () => {
-  let choice = prompt("Choose Rock, Paper, or Scissors")
-  choice = choice.toUpperCase()
-  return choice 
-}
-
 let humanScore = 0;
 let computerScore = 0;
 
-const playGame = () => {
+const getComputerChoice = () => {
+  const randomNum = Math.floor(Math.random() * 3) + 1;
 
-  for(i = 0; i < 5; i++){
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-
-const playRound = (humanChoice, computerChoice) => {
-
-  if (humanChoice === computerChoice) {
-    console.log("It's a tie, try your luck again!")
-    console.log(`Human Score:${humanScore} Computer Score ${computerScore}`)
-    
+  if (randomNum === 1) {
+    return "ROCK";
+  } else if (randomNum === 2) {
+    return "PAPER";
+  } else {
+    return "SCISSORS";
   }
- else if ((humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
-         (humanChoice === "PAPER" && computerChoice === "ROCK") ||
-         (humanChoice === "SCISSORS" && computerChoice === "PAPER")) {
-          console.log(`You win, ${humanChoice} beats ${computerChoice}`);
-          humanScore++;
-          console.log(`Human Score:${humanScore} Computer Score ${computerScore}`)
-         }
-        else {
-          console.log(`You lose, ${computerChoice} beats ${humanChoice}, computer wins`);
-          computerScore++;
-          console.log(`Human Score:${humanScore} Computer Score ${computerScore}`)
-
-        }
 };
 
-playRound(humanChoice, computerChoice)
+const playRound = (humanChoice, computerChoice) => {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie, try your luck again!");
+    console.log(`Human Score: ${humanScore} Computer Score: ${computerScore}`);
+  } else if (
+    (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
+    (humanChoice === "PAPER" && computerChoice === "ROCK") ||
+    (humanChoice === "SCISSORS" && computerChoice === "PAPER")
+  ) {
+    humanScore++;
+  } else {
+    computerScore++;
   }
-}
+};
 
-playGame()
-*/
-
-
+getHumanChoice();
 
 
